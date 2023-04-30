590cdb31bf0f8c35ef86f1cae3ca1cf21bd388a8
debanjo31 <debanjo31@gmail.com>
2023-04-30 13:57:27 +0100

    styling the header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,11 +4,13 @@ import { FaBookOpen } from "react-icons/fa";
 function Header() {
   return (
     <div className="header">
-      <h2 style={{ marginRight: "10px" }}>
-        {" "}
-        <FaBookOpen style={{ verticalAlign: "sub" }} />
-      </h2>
-      <h2>CPGA calculator</h2>
+      <div className="text">
+        <h2 style={{ marginRight: "10px" }}>
+          {" "}
+          <FaBookOpen style={{ verticalAlign: "sub" }} />
+        </h2>
+        <h2>CPGA calculator</h2>
+      </div>
     </div>
   );
 }
